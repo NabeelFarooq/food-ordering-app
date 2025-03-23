5bdd4c44b2dca04fee453859aab33e0471f355f5
Nabeel <nabeel007dar@gmail.com>
2025-03-23 21:01:43 +0530

    Apply routing and make restaurant cards clickable to fetch data dynamically

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,30 +1,54 @@
-import { useEffect } from "react";
-
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
   useEffect(() => {
     fetchMenu();
   }, []);
 
+  const { resId } = useParams();
+  console.log(resId);
   const fetchMenu = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=342547&catalog_qa=undefined&metaData=%7B%22type%22%3A%22RESTAURANT%22%2C%22data%22%3A%7B%22parentId%22%3A635%2C%22primaryRestaurantId%22%3A229%2C%22cloudinaryId%22%3A%22xqwpuhgnsaf18te7zvtv%22%2C%22brandId%22%3A635%2C%22dishFamilyId%22%3A%22846613%22%2C%22enabled_flag%22%3A1%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Restaurant%22%7D&submitAction=SUGGESTION"
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId=" +
+        resId +
+        "&catalog_qa=undefined&metaData=%7B%22type%22%3A%22RESTAURANT%22%2C%22data%22%3A%7B%22parentId%22%3A635%2C%22primaryRestaurantId%22%3A229%2C%22cloudinaryId%22%3A%22xqwpuhgnsaf18te7zvtv%22%2C%22brandId%22%3A635%2C%22dishFamilyId%22%3A%22846613%22%2C%22enabled_flag%22%3A1%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Restaurant%22%7D&submitAction=SUGGESTION"
     );
     const json = await data.json();
 
     console.log(json);
+
+    setResInfo(json.data);
   };
 
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[2]?.card?.card?.info;
+  const { itemCards } =
+    resInfo?.cards[4]?.groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
+  console.log(itemCards);
   return (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
       <h2>Menu</h2>
       <ul>
-        <li>Biryani</li>
-        <li>Burgers</li>
-        <li>Diet Coke</li>
+        {itemCards.map((item) => (
+          <li key="item.card.info.id">
+            {item.card.info.name} - ₹
+            {parseFloat(item.card.info.price) / 100 ||
+              parseFloat(item.card.info.defaultPrice) / 100}
+          </li>
+        ))}
       </ul>
     </div>
   );
 };
-
 export default RestaurantMenu;
