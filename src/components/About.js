b802d33d37949a255997be6c26ce99a64e73552b
Nabeel <nabeel007dar@gmail.com>
2025-03-25 14:54:30 +0530

    Add Class Components to project

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,28 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>This is about page of food ordering app</h2>
-    </div>
-  );
-};
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent Constructor");
+  }
+
+  componentDidMount() {
+    console.log("Parent Component Did Mount");
+  }
+  render() {
+    console.log("Parent Render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is about page of food ordering app</h2>
+        <UserClass
+          name={"Nabeel Farooq (class)"}
+          location={"Srinagar (class)"}
+        />
+      </div>
+    );
+  }
+}
 
 export default About;
