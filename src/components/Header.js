fcb24b2ba34574784c8bdb00a81bc9913b4b7ebf
Nabeel <nabeel007dar@gmail.com>
2025-04-04 14:22:48 +0530

    Add test cases for header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   // console.log(loggedInUser);
 
   const cartItems = useSelector((store) => store.cart.items);
-  console.log(cartItems);
+  // console.log(cartItems);
 
   return (
     <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
---
+++ src/components/__tests__/Header.test.js
@@ -0,0 +1,61 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Header from "../Header";
+import "@testing-library/jest-dom";
+import { Provider } from "react-redux";
+import appStore from "../../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+it("Should render header Component with a login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  //   const loginButton = screen.getByText("Login");
+  expect(loginButton).toBeInTheDocument();
+});
+
+it("Should render header Component with a Cart items 0", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItems = screen.getByText("Cart - (0 items)");
+  //   const loginButton = screen.getByText("Login");
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("Should render header Component with a Cart item", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItems = screen.getByText(/Cart/);
+  expect(cartItems).toBeInTheDocument();
+});
+
+it("Should change Login button to Logout on click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(loginButton);
+  const logoutButton = screen.getByRole("button", { name: "Logout" });
+  expect(logoutButton).toBeInTheDocument();
+});
