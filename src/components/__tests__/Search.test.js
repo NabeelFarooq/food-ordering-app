bb4897e516c5c455d008f46526c0d0e7d223dffd
Nabeel <nabeel007dar@gmail.com>
2025-04-04 18:24:06 +0530

    Add test case for top rated restaurants feature

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -33,3 +33,25 @@ it("Should Search Res List for burger text input", async () => {
   const cardsAfterSearch = screen.getAllByTestId("resCard");
   expect(cardsAfterSearch.length).toBe(1);
 });
+
+it("Should filter Top Rated Restaurant ", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
+
+  const cardsBeforeFilter = screen.getAllByTestId("resCard");
+  expect(cardsBeforeFilter.length).toBe(8);
+
+  const topRatedBtn = screen.getByRole("button", {
+    name: "Top Rated Restaurants",
+  });
+
+  fireEvent.click(topRatedBtn);
+
+  const cardsAfterFilter = screen.getAllByTestId("resCard");
+  expect(cardsAfterFilter.length).toBe(3);
+});
