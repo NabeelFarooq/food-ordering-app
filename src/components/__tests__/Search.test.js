f5946d92aebda2c869f14506b3fbed85b72c7d22
Nabeel <nabeel007dar@gmail.com>
2025-04-04 17:39:00 +0530

    Add test cases for search functionality

---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,35 @@
+import { fireEvent, render } from "@testing-library/react";
+import Body from "../Body";
+import MOCK_DATA from "../mocks/mockResListData.json";
+import { act } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+import { screen } from "@testing-library/react";
+global.fetch = jest.fn(() => {
+  return Promise.resolve({
+    json: () => {
+      return Promise.resolve(MOCK_DATA);
+    },
+  });
+});
+
+it("Should Search Res List for burger text input", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>
+    )
+  );
+
+  const cardsBeforeSearch = screen.getAllByTestId("resCard");
+  expect(cardsBeforeSearch.length).toBe(8);
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+  const searchInput = screen.getByTestId("searchInput");
+  fireEvent.change(searchInput, {
+    target: { value: "burger" },
+  });
+  fireEvent.click(searchBtn);
+  const cardsAfterSearch = screen.getAllByTestId("resCard");
+  expect(cardsAfterSearch.length).toBe(1);
+});
